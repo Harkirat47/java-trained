b2bb55f08fcbe5c9da182fca342d3945eda15dc2
Harkirat47 <har.ki.kirat@gmail.com>
2022-09-28 08:56:00 -0700

    Array hw

---
+++ Week 2/Arrays/Array.java
@@ -0,0 +1,14 @@
+public class Array {
+    public static void main(String[] args) {
+        String[] Array= new String[4];
+        Array[0]="softwere is the best ";
+        Array[1]="Jeanne Calment";
+        Array[2]="I dont know, and you cant make me ";
+        Array[3]="Deep space";
+        for (int i = 0; i < Array.length; i++) {
+            System.out.println(Array[i]);
+        }
+
+    }
+    
+}
