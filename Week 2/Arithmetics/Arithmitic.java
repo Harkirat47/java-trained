4b9638c10681434d8eaf34699ff42c433730a5a7
Harkirat47 <har.ki.kirat@gmail.com>
2022-09-28 08:27:20 -0700

    done with arithmitic

--- Week 2/Arithmetics/Arithmitic.java
+++ Week 2/Arithmetics/Arithmitic.java
@@ -1,5 +1,8 @@
 public class Arithmitic {
     public static void main(String[] args) {
-        
+        // Compute the remainder of 6 multiplied by 1234124 divided by 11345 minus 890809 plus 90800 (use order of operations) is 
+        //divided by 980098, and store this in a variable called num 
+        double num = (((6 * 1234124)%11345)-890809+90800) / 980098 ;
+        System.out.println(num/100);
     }
 }
